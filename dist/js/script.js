daac7279de4138e18c0e88dce7c6bc5c6ab0240d
ibnunazm <rosyidkrenz1101@gmail.com>
2023-09-16 23:48:24 +0700

    benerin

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,16 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector('#to-top');
+  const toTop = document.querySelector("#to-top");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
-    toTop.classList.remove('flex');
-    toTop.classList.add('hidden');
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
@@ -25,9 +25,9 @@ hamburger.addEventListener("click", function () {
 });
 
 // Klik di luar hamburger
-window.addEventListener('click', function (e) {
-    if (e.target !== hamburger && e.target !== navMenu) {
-      hamburger.classList.remove('hamburger-active');
-      navMenu.classList.add('hidden');
-    }
-  });
+window.addEventListener("click", function (e) {
+  if (e.target !== hamburger && e.target !== navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
